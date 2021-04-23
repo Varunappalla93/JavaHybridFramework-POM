626c9fd5bb695d02a00cd14dbfb29602dee4b557
varunappalla93 <an.varun1993@gmail.com>
2021-04-23 15:15:59 +0530

    addtocart page added

---
+++ src/main/java/com/qa/opencart/pages/AddtoCartPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AddtoCartPage {
+
+	WebDriver driver;
+	
+	public AddtoCartPage(WebDriver driver) {
+		this.driver=driver;
+	}
+}
